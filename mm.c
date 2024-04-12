292109775b2f7c4908035e9c105b015588d280c6
Yoosun-Chang <12201321@inha.edu>
2024-04-12 02:17:53 +0000

    Feat: 기본 상수와 매크로 선언(#1)

--- mm.c
+++ mm.c
@@ -36,30 +36,22 @@ team_t team = {
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 
-/* 기본 상수와 매크로 */
-#define WSIZE 4             // word size
-#define DSIZE 8             // double word size
-#define CHUNKSIZE (1 << 12) // 힙 확장을 위한 기본 크기 (= 초기 빈 블록의 크기, 4kb)
+/* 기본 상수 */
+#define WSIZE 4          
+#define DSIZE 8             
+#define CHUNKSIZE (1 << 12)
 
-#define MAX(x, y) ((x) > (y) ? (x) : (y)) // x,y 중 큰 값
-
-/* 가용 리스트에 접근하고 방문하는 작은 매크로들 */
-#define PACK(size, alloc) ((size) | (alloc)) // 크기와 할당 비트를 통합 -> 헤더와 푸터에 저장
-// alloc : 가용여부 (ex. 000) / size : block size를 의미. =>합치면 온전한 주소가 나온다.
-
-#define GET(p) (*(unsigned int *)(p)) // p가 참조하는 워드 리턴 / 인자 p는 (void*) 이므로 역참조는 불가하다.
-#define PUT(p, val) (*(unsigned int *)(p) = (val)) // p에 val을 저장
-
-#define GET_SIZE(p) (GET(p) & ~0x7) // 헤더 또는 푸터의 사이즈 리턴
-#define GET_ALLOC(p) (GET(p) & 0x1) // 가용 여부 리턴
-
-#define HDRP(bp) ((char *)(bp) - WSIZE) // 블록 헤더를 가리키는 포인터 리턴
-#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE) // 블록 푸터를 가리키는 포인터 리턴
-// ↳ HDRP(bp)에 헤더 사이즈(w)도 포함되어 있어서 DSIZE를 빼준다.
-
-#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE))) // 다음 블록의 포인터
-#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE))) // 이전 블록의 포인터
-
+/* 매크로 */
+#define MAX(x, y) ((x) > (y) ? (x) : (y)) 
+#define PACK(size, alloc) ((size) | (alloc)) 
+#define GET(p) (*(unsigned int *)(p)) 
+#define PUT(p, val) (*(unsigned int *)(p) = (val)) 
+#define GET_SIZE(p) (GET(p) & ~0x7) 
+#define GET_ALLOC(p) (GET(p) & 0x1) 
+#define HDRP(bp) ((char *)(bp) - WSIZE) 
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE))) 
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE))) 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
 /* 
