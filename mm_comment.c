8749fd527159e691432627946f79fa99a7c19e4c
Yoosun-Chang <12201321@inha.edu>
2024-04-15 06:46:32 +0000

    Comment: 오타 수정

--- mm_comment.c
+++ mm_comment.c
@@ -71,7 +71,7 @@ static char *next_heap_listp;
 
 /* 
  * mm_init - initialize the malloc package.
- * 최초의 가용블록(4words)을 가지고 힙b  을 생성하고 할당기를 초기화한다. 
+ * 최초의 가용블록(4words)을 가지고 힙을 생성하고 할당기를 초기화한다. 
  */
 int mm_init(void)
 {
